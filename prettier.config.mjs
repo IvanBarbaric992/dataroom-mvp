3ba2e168a3493bd7349733c7e367a8ccf05d6c86
Ivan Barbaric <ivan.barbaric992@gmail.com>
2025-10-03 15:56:05 +0200

    fix: fix prettier format problem

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,14 @@
-/** @type {import('prettier').Config} */
-export default {
+/**
+ * @see https://prettier.io/docs/en/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
   printWidth: 100,
   singleQuote: true,
   semi: true,
   trailingComma: 'all',
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindStylesheet: './src/index.css',
+  tailwindStylesheet: 'src/index.css',
 };
+
+export default config;
